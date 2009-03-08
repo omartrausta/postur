cb15b5edddcf36b704198610ed830ff756730cb7
davidhalldor <davidhalldor@53ea51bc-0993-11de-b753-7319d013a8fa>
2009-03-08 15:42:00 +0000

    Updated version of the pop3 ruby mail client

--- src/MailClient.rb
+++ src/MailClient.rb
@@ -1,11 +1,16 @@
 require 'net/pop'
 
-pop = Net::POP3.new('192.168.1.34')
-pop.start('dabbi', '1234') do |server|
+if ARGV.size <= 3
+  puts 'Use: server port user pass'
+  exit!
+end
 
-  for msg in server.mails 
-    puts 'Mail messages:'   
-    text = msg.pop
+pop = Net::POP3.new(ARGV[0],ARGV[1])     #Connects to POP3 Server
+pop.start(ARGV[2],ARGV[3]) do |server|   #Login with user and pass
+  for msg in server.mails                #Iterates through mails
+    puts 'Mail messages:'
+    text = msg.pop                       #text gets first mail in server.mails array
     puts text
+    msg.delete                           #delete post in inbox
   end
 end
