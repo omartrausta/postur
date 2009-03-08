3146680b5ab1093a5c903c32509df40af6339db0
davidhalldor <davidhalldor@53ea51bc-0993-11de-b753-7319d013a8fa>
2009-03-08 15:53:01 +0000

    Commented properly :)

--- trunk/src/MailClient.rb
+++ trunk/src/MailClient.rb
@@ -1,3 +1,8 @@
+=begin
+Author: Davíð Halldór Lúðvíksson
+Changed: 8.03.09
+Licence: MIT
+=end
 require 'net/pop'
 
 if ARGV.size <= 3
@@ -5,12 +10,12 @@ if ARGV.size <= 3
   exit!
 end
 
-pop = Net::POP3.new(ARGV[0],ARGV[1])     #Connects to POP3 Server
-pop.start(ARGV[2],ARGV[3]) do |server|   #Login with user and pass
-  for msg in server.mails                #Iterates through mails
+pop = Net::POP3.new(ARGV[0],ARGV[1])		#connects to POP3 Server
+pop.start(ARGV[2],ARGV[3]) do |server|		#login with user and pass
+  for msg in server.mails					#iterates through mails
     puts 'Mail messages:'
-    text = msg.pop                       #text gets first mail in server.mails array
-    puts text
-    msg.delete                           #delete post in inbox
+    text = msg.pop							#text gets mail in server.mails array
+    puts text								#writes mail to console
+    msg.delete								#delete post in inbox
   end
 end
