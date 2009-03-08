9afc74aafa5023d2128d50e2c2a6dc87e4977602
davidhalldor <davidhalldor@53ea51bc-0993-11de-b753-7319d013a8fa>
2009-03-08 15:59:36 +0000

    trying to fix comments

--- src/MailClient.rb
+++ src/MailClient.rb
@@ -10,12 +10,12 @@ if ARGV.size <= 3
   exit!
 end
 
-pop = Net::POP3.new(ARGV[0],ARGV[1])	#connects to POP3 Server
-pop.start(ARGV[2],ARGV[3]) do |server|	#login with user and pass
-  for msg in server.mails				#iterates through mails
+pop = Net::POP3.new(ARGV[0],ARGV[1])                                            #connects to POP3 Server
+pop.start(ARGV[2],ARGV[3]) do |server|                                          #login with user and pass
+  for msg in server.mails                                                       #iterates through mails
     puts 'Mail messages:'
-    text = msg.pop						#text gets mail in server.mails array
-    puts text							#writes mail to console
-    msg.delete							#delete post in inbox
+    text = msg.pop                                                              #text gets mail in server.mails array
+    puts text                                                                   #writes mail to console
+    msg.delete                                                                  #delete post in inbox
   end
 end
